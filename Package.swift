5a253e17072572a9d6624adbf0e204d8fc09bf5b
CatchZeng <891793848@qq.com>
2020-11-23 16:47:16 +0800

    fix: SPM exclude images

--- Package.swift
+++ Package.swift
@@ -21,9 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Marquee",
-            dependencies: []),
-        .testTarget(
-            name: "MarqueeTests",
-            dependencies: ["Marquee"]),
+            dependencies: [],
+            exclude: ["images"]),
     ]
 )
